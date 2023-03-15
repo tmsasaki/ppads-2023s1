0e947341e26563c9eb6d52cfc61b24a079fe0b2a
Gacarvalho <Gabrielbco25@gmail.com>
2023-03-14 21:33:07 -0300

    Criação da estrutura incial do código do back para tela de login

--- src/siteback/src/app.module.ts
+++ src/siteback/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DisciplinaModule } from './disciplina/disciplina.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DisciplinaModule],
   controllers: [AppController],
   providers: [AppService],
 })
