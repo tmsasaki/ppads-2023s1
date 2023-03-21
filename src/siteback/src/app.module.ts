f05d0e0f78afaa017b3bc7bc27b3ff2520941111
Gacarvalho <Gabrielbco25@gmail.com>
2023-03-21 12:42:26 -0300

    adição de componentes para o backend

--- src/siteback/src/app.module.ts
+++ src/siteback/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import { DisciplinaModule } from './disciplina/disciplina.module';
+import { professorModule } from './professor/user.module';
 
 @Module({
-  imports: [UserModule, DisciplinaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, professorModule, DisciplinaModule],
+
 })
 export class AppModule {}
