72b28f2f930eff5872d4215a0c361206da955142
Gacarvalho29 <Gabrielbco25@gmail.com>
2023-03-21 17:53:35 -0300

    Mais adição da funcionalidade de Login

--- src/siteback/src/app.module.ts
+++ src/siteback/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DisciplinaModule } from './disciplina/disciplina.module';
 import { professorModule } from './professor/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+
 
 @Module({
-  imports: [AuthModule, professorModule, DisciplinaModule],
+  imports: [AuthModule, professorModule, DisciplinaModule, PrismaModule],
 
 })
 export class AppModule {}
--- src/siteback/src/auth/auth.service.ts
+++ src/siteback/src/auth/auth.service.ts
@@ -1,13 +1,44 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-    
-    signup() {
-        return {msg: 'eu estou logando'};
+    constructor(private prisma: PrismaService){}
+
+    async signup(dto: AuthDto) {
+        //generate password hash
+        const hash = await argon.hash(dto.password);
+        //save new user in db
+        
+        const user = await this.prisma.user.create({
+            data:{
+                email: dto.email,
+                hash,
+            },
+        })
+
+        delete user.hash;
+
+        // return saved user
+        return user;
     }
 
-    signin() {
+    async signin(dto: AuthDto) {
+        //find user email
+        const user = 
+            await this.prisma.user.findUnique({
+                where: {
+                    email: dto.email,
+                },
+            });
+        //if user does not exist throw exeception
+
+        //compare password
+        //if password incorret throw exception
+
+        //return user
         return {msg: 'i estou logado'}
     }
 }
